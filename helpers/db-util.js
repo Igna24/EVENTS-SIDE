cc4e6ddc9cc7900376429446ed86d85f70068b5d
Ignacio Marin <ignaciomaringarro@gmail.com>
2023-11-18 10:36:22 -0600

    vercel to mongoDB

--- helpers/db-util.js
+++ helpers/db-util.js
@@ -1,25 +1,28 @@
 import { MongoClient } from "mongodb";
 
-export async function connectDatabase() {
-  const client = await MongoClient.connect(
-    "mongodb+srv://Cluster49474:Bjbwu408kbvV6Ysx@cluster49474.g1yfmro.mongodb.net/?retryWrites=true&w=majority" // this has to be added for proper operation.
-  );
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
+let cachedClient = null;
+
+export async function connectDatabase() {
+  if (cachedClient) {
+    return cachedClient;
+  }
+  const client = await MongoClient.connect(process.env.MONGODB_URI);
+  cachedClient = client;
   return client;
 }
 
 export async function insertDocument(client, collection, document) {
   const db = client.db();
-
   const result = await db.collection(collection).insertOne(document);
-
   return result;
 }
 
 export async function getAllDocuments(client, collection, sort) {
   const db = client.db();
-
   const documents = await db.collection(collection).find().sort(sort).toArray();
-
   return documents;
 }
